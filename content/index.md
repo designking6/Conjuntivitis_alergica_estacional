06a536e0a0fd4cfe549482f63537390b4914c14c
designking6 <echo_work.01@proton.me>
2026-02-11 15:17:16 -0600

    Quartz sync: Feb 11, 2026, 3:17 PM

--- content/index.md
+++ content/index.md
@@ -5,4 +5,3 @@
 → \[\[SAC/Conjuntivitis alérgica estacionaria]]
 
 
-
--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Roboto Slab",
+        body: "Rubik",
         code: "IBM Plex Mono",
       },
       colors: {
