a7d32124b9e93d9dde080a9588ab3a38c6c8a4ff
designking6 <echo_work.01@proton.me>
2026-02-11 15:27:52 -0600

    Quartz sync: Feb 11, 2026, 3:27 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Roboto Slab",
-        body: "Rubik",
+        header: "Inter",
+        body: "Inter",
         code: "IBM Plex Mono",
       },
       colors: {
